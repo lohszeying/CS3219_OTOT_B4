c76e5e7decebc0beb2d67a2cbf5cb21c5bb026be
Loh Sze Ying <bloom9334@hotmail.com>
2020-10-04 18:26:29 +0800

    first commit

---
+++ contactModel.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+// Setup schema
+var contactSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+// Export Contact model
+var Contact = module.exports = mongoose.model('contact', contactSchema);
+module.exports.get = function (callback, limit) {
+    Contact.find(callback).limit(limit);
+}
---
+++ tests/test.js
@@ -0,0 +1,60 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+import app from "../index.js";
+let id;
+
+chai.use(chaiHttp);
+chai.should();
+
+describe("contacts", () => {
+	describe("GET /", () => {
+		//Test to get all contacts record
+		it("should get all contacts record", (done) => {
+            chai.request(app)
+                .get('/api/contacts')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                 });
+        });
+    });
+    
+    describe("POST /", () => {
+		//Test to get all contacts record
+		it("should create a new contact", (done) => {
+            chai.request(app)
+                .post('/api/contacts')
+                .send({name: "John Day"})
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    id = res.body.data._id;
+                    done();
+                 });
+        });
+    });
+    
+    describe("PUT /", () => {
+		//Test to get all contacts record
+		it("should update 1 contact", (done) => {
+            chai.request(app)
+                .put('/api/contacts/' + id)
+                .send({name: "John Dayes"})
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                 });
+        });
+    });
+
+    describe("PUT /", () => {
+		//Test to get all contacts record
+		it("should delete 1 contact", (done) => {
+            chai.request(app)
+                .delete('/api/contacts/' + id)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                 });
+        });
+    });
+});
